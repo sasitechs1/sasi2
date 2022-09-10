d93f05cce912c315e309b2a68dbfdbbb05aafe6d
sasi0328 <sasikumarreddym98@gmail.com>
2022-09-10 08:21:59 +0000

    reg

--- viji.sh
+++ viji.sh
@@ -6,8 +6,8 @@ echo "the  addition value of user numbers  is  ` expr $a + $b `"
 echo "the multiply of two numbers is :`expr $a \* $b `"
 echo "the division vaue of two values are: `expr $a / $b` "
 echo "the sub value of two numbers are :`expr $a - $b`"
->>>>>>HEAD
-date 
-<<<<<<MASTER
+cal
+
+date
 
 
