0f8ffb685273d661e5b6da2530505eb4cf991694
sasi0328 <sasikumarreddym98@gmail.com>
2022-09-10 07:40:17 +0000

    ser

---
+++ viji.sh
@@ -0,0 +1,10 @@
+read -p 'Enter The Value ' a
+read -p 'Enter the value ' b
+echo `       `
+echo "the sub of the value is :"`expr $a - $b ` 
+echo "the  addition value of user numbers  is  ` expr $a + $b `"
+echo "the multiply of two numbers is :`expr $a \* $b `"
+echo "the division vaue of two values are: `expr $a / $b` "
+echo "the sub value of two numbers are :`expr $a - $b`"
+date 
+
