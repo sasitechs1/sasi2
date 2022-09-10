a620db0830c50e050d6f658ab79534f03052a7c8
sasi0328 <sasikumarreddym98@gmail.com>
2022-09-10 07:40:17 +0000

    vji

--- viji.sh
+++ viji.sh
@@ -6,8 +6,12 @@ echo "the  addition value of user numbers  is  ` expr $a + $b `"
 echo "the multiply of two numbers is :`expr $a \* $b `"
 echo "the division vaue of two values are: `expr $a / $b` "
 echo "the sub value of two numbers are :`expr $a - $b`"
+<<<<<<< HEAD
 cal
 
 date
 
+=======
+date 
+>>>>>>> 0f8ffb6 (ser)
 
